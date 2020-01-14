1893fc9d43449d296abb042805f14157a946b2d6
Apple <bryantconan@yahoo.co.jp>
2020-01-14 17:15:15 +0800

    添加文件内容变更提示符(星号*)。

--- NotePad.vb
+++ NotePad.vb
@@ -1,9 +1,16 @@
-﻿Public Class NotePad
+﻿Imports System.IO
 
-    Private filePath As String = Nothing  ' 当前文件的文件名(具体路径)
+Public Class NotePad
+
+    Private filePath As String = Nothing  ' 当前文件具体路径
+    Private fileName As String = Nothing  ' 当前文件的文件名
     Private fileNumber As Integer  ' 当前文件的文件号码
     Private currentContent As String  ' 原文件缓存内容：用于比较当前文本框内容和文件内容是否一致
     Private isEdited As Boolean = False  ' 文件内容变更标识符
+
+    Private Const NOTE_PAD As String = " - 记事本"  ' 记事本
+    Private Const ASTER As String = "*"  ' 星号：用来提示文本内容发生变化
+    Private Const NO_TITLE As String = "无标题"  ' 无标题
 
     ' 保存文件
     Private Sub MI_Save_Click(sender As Object, e As EventArgs) Handles MI_Save.Click
@@ -16,6 +23,10 @@
             PrintLine(Me.fileNumber, TB_Editor.Text)  ' 把文本框的内容逐行写入文件
             FileClose(Me.fileNumber)  ' 关闭文件
             Me.isEdited = False
+
+            ' 变更窗口的标题内容
+            Me.fileName = Path.GetFileName(Me.filePath)  ' 获取当前文件的文件名(含扩展名，不含路径)
+            Me.Text = Me.fileName & NOTE_PAD
         End If
 
     End Sub
@@ -41,6 +52,10 @@
         Loop
         FileClose(Me.fileNumber)  ' 关闭文件
         Me.isEdited = False
+
+        ' 变更窗口的标题内容
+        Me.fileName = Path.GetFileName(Me.filePath)  ' 获取当前文件的文件名(含扩展名，不含路径)
+        Me.Text = Me.fileName & NOTE_PAD
     End Sub
 
     ' 退出
@@ -61,6 +76,10 @@
         PrintLine(Me.fileNumber, TB_Editor.Text)  ' 把文本框的内容逐行写入文件
         FileClose(Me.fileNumber)  ' 关闭文件
         Me.isEdited = False
+
+        ' 变更窗口的标题内容
+        Me.fileName = Path.GetFileName(Me.filePath)  ' 获取当前文件的文件名(含扩展名，不含路径)
+        Me.Text = Me.fileName & NOTE_PAD
     End Sub
 
     ' 新建
@@ -71,7 +90,9 @@
 
     ' 窗口加载事件
     Private Sub NotePad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        If Me.filePath = Nothing Then
+            Me.Text = NO_TITLE & NOTE_PAD
+        End If
     End Sub
 
     ' 窗口关闭事件
@@ -88,6 +109,13 @@
     Private Sub TB_Editor_TextChanged(sender As Object, e As EventArgs) Handles TB_Editor.TextChanged
         If Equals(currentContent, TB_Editor.Text) = False Then  ' 文本框内容有变更的话
             Me.isEdited = True
+
+            ' 变更窗口的标题内容
+            If Me.filePath = Nothing Then
+                Me.Text = ASTER & NO_TITLE & NOTE_PAD
+            Else
+                Me.Text = ASTER & Me.fileName & NOTE_PAD
+            End If
         End If
     End Sub
 End Class
