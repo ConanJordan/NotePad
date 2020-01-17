5b40f86e24b2b49d6f6c05a691b3860b06876c19
Apple <bryantconan@yahoo.co.jp>
2020-01-17 09:56:32 +0800

    修改【替换】对话框的控件的变量名。

--- ReplaceDialog.Designer.vb
+++ ReplaceDialog.Designer.vb
@@ -22,128 +22,129 @@ Partial Class ReplaceDialog
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.LB_FindContent = New System.Windows.Forms.Label()
+        Me.LB_ReplaceContent = New System.Windows.Forms.Label()
+        Me.TB_FindContent = New System.Windows.Forms.TextBox()
+        Me.TB_ReplaceContent = New System.Windows.Forms.TextBox()
+        Me.BTN_Next = New System.Windows.Forms.Button()
+        Me.BTN_Replace = New System.Windows.Forms.Button()
+        Me.BTN_ReplaceAll = New System.Windows.Forms.Button()
+        Me.BTN_Cancel = New System.Windows.Forms.Button()
+        Me.CB_IsCaseSensitive = New System.Windows.Forms.CheckBox()
+        Me.CB_IsLoop = New System.Windows.Forms.CheckBox()
         Me.SuspendLayout()
         '
-        'Label1
+        'LB_FindContent
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Label1.Location = New System.Drawing.Point(0, 21)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(93, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "查找内容(&N)："
+        Me.LB_FindContent.AutoSize = True
+        Me.LB_FindContent.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.LB_FindContent.Location = New System.Drawing.Point(0, 21)
+        Me.LB_FindContent.Name = "LB_FindContent"
+        Me.LB_FindContent.Size = New System.Drawing.Size(93, 13)
+        Me.LB_FindContent.TabIndex = 0
+        Me.LB_FindContent.Text = "查找内容(&N)："
         '
-        'Label2
+        'LB_ReplaceContent
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Label2.Location = New System.Drawing.Point(0, 63)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(80, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "替换为(&P)："
+        Me.LB_ReplaceContent.AutoSize = True
+        Me.LB_ReplaceContent.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.LB_ReplaceContent.Location = New System.Drawing.Point(0, 63)
+        Me.LB_ReplaceContent.Name = "LB_ReplaceContent"
+        Me.LB_ReplaceContent.Size = New System.Drawing.Size(80, 13)
+        Me.LB_ReplaceContent.TabIndex = 1
+        Me.LB_ReplaceContent.Text = "替换为(&P)："
         '
-        'TextBox1
+        'TB_FindContent
         '
-        Me.TextBox1.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(99, 19)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(250, 22)
-        Me.TextBox1.TabIndex = 2
+        Me.TB_FindContent.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TB_FindContent.Location = New System.Drawing.Point(99, 19)
+        Me.TB_FindContent.Name = "TB_FindContent"
+        Me.TB_FindContent.Size = New System.Drawing.Size(250, 22)
+        Me.TB_FindContent.TabIndex = 2
         '
-        'TextBox2
+        'TB_ReplaceContent
         '
-        Me.TextBox2.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(99, 60)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(250, 22)
-        Me.TextBox2.TabIndex = 3
+        Me.TB_ReplaceContent.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TB_ReplaceContent.Location = New System.Drawing.Point(99, 60)
+        Me.TB_ReplaceContent.Name = "TB_ReplaceContent"
+        Me.TB_ReplaceContent.Size = New System.Drawing.Size(250, 22)
+        Me.TB_ReplaceContent.TabIndex = 3
         '
-        'Button1
+        'BTN_Next
         '
-        Me.Button1.Location = New System.Drawing.Point(364, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(108, 26)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "查找下一个(&F)"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.BTN_Next.Location = New System.Drawing.Point(364, 12)
+        Me.BTN_Next.Name = "BTN_Next"
+        Me.BTN_Next.Size = New System.Drawing.Size(108, 26)
+        Me.BTN_Next.TabIndex = 4
+        Me.BTN_Next.Text = "查找下一个(&F)"
+        Me.BTN_Next.UseVisualStyleBackColor = True
         '
-        'Button2
+        'BTN_Replace
         '
-        Me.Button2.Location = New System.Drawing.Point(364, 52)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(108, 26)
-        Me.Button2.TabIndex = 5
-        Me.Button2.Text = "替换(&R)"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.BTN_Replace.Location = New System.Drawing.Point(364, 52)
+        Me.BTN_Replace.Name = "BTN_Replace"
+        Me.BTN_Replace.Size = New System.Drawing.Size(108, 26)
+        Me.BTN_Replace.TabIndex = 5
+        Me.BTN_Replace.Text = "替换(&R)"
+        Me.BTN_Replace.UseVisualStyleBackColor = True
         '
-        'Button3
+        'BTN_ReplaceAll
         '
-        Me.Button3.Location = New System.Drawing.Point(364, 92)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(108, 26)
-        Me.Button3.TabIndex = 6
-        Me.Button3.Text = "全部替换(&A)"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.BTN_ReplaceAll.Location = New System.Drawing.Point(364, 92)
+        Me.BTN_ReplaceAll.Name = "BTN_ReplaceAll"
+        Me.BTN_ReplaceAll.Size = New System.Drawing.Size(108, 26)
+        Me.BTN_ReplaceAll.TabIndex = 6
+        Me.BTN_ReplaceAll.Text = "全部替换(&A)"
+        Me.BTN_ReplaceAll.UseVisualStyleBackColor = True
         '
-        'Button4
+        'BTN_Cancel
         '
-        Me.Button4.Location = New System.Drawing.Point(364, 132)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(108, 26)
-        Me.Button4.TabIndex = 7
-        Me.Button4.Text = "取消"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.BTN_Cancel.Location = New System.Drawing.Point(364, 132)
+        Me.BTN_Cancel.Name = "BTN_Cancel"
+        Me.BTN_Cancel.Size = New System.Drawing.Size(108, 26)
+        Me.BTN_Cancel.TabIndex = 7
+        Me.BTN_Cancel.Text = "取消"
+        Me.BTN_Cancel.UseVisualStyleBackColor = True
         '
-        'CheckBox1
+        'CB_IsCaseSensitive
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.CheckBox1.Location = New System.Drawing.Point(12, 154)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(112, 17)
-        Me.CheckBox1.TabIndex = 8
-        Me.CheckBox1.Text = "区分大小写(&C)"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.CB_IsCaseSensitive.AutoSize = True
+        Me.CB_IsCaseSensitive.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.CB_IsCaseSensitive.Location = New System.Drawing.Point(12, 154)
+        Me.CB_IsCaseSensitive.Name = "CB_IsCaseSensitive"
+        Me.CB_IsCaseSensitive.Size = New System.Drawing.Size(112, 17)
+        Me.CB_IsCaseSensitive.TabIndex = 8
+        Me.CB_IsCaseSensitive.Text = "区分大小写(&C)"
+        Me.CB_IsCaseSensitive.UseVisualStyleBackColor = True
         '
-        'CheckBox2
+        'CB_IsLoop
         '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.CheckBox2.Location = New System.Drawing.Point(12, 187)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(73, 17)
-        Me.CheckBox2.TabIndex = 9
-        Me.CheckBox2.Text = "循环(&R)"
-        Me.CheckBox2.UseVisualStyleBackColor = True
+        Me.CB_IsLoop.AutoSize = True
+        Me.CB_IsLoop.Font = New System.Drawing.Font("宋体", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.CB_IsLoop.Location = New System.Drawing.Point(12, 187)
+        Me.CB_IsLoop.Name = "CB_IsLoop"
+        Me.CB_IsLoop.Size = New System.Drawing.Size(73, 17)
+        Me.CB_IsLoop.TabIndex = 9
+        Me.CB_IsLoop.Text = "循环(&R)"
+        Me.CB_IsLoop.UseVisualStyleBackColor = True
         '
         'ReplaceDialog
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(484, 211)
-        Me.Controls.Add(Me.CheckBox2)
-        Me.Controls.Add(Me.CheckBox1)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CB_IsLoop)
+        Me.Controls.Add(Me.CB_IsCaseSensitive)
+        Me.Controls.Add(Me.BTN_Cancel)
+        Me.Controls.Add(Me.BTN_ReplaceAll)
+        Me.Controls.Add(Me.BTN_Replace)
+        Me.Controls.Add(Me.BTN_Next)
+        Me.Controls.Add(Me.TB_ReplaceContent)
+        Me.Controls.Add(Me.TB_FindContent)
+        Me.Controls.Add(Me.LB_ReplaceContent)
+        Me.Controls.Add(Me.LB_FindContent)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.KeyPreview = True
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "ReplaceDialog"
@@ -154,14 +155,14 @@ Partial Class ReplaceDialog
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents LB_FindContent As Label
+    Friend WithEvents LB_ReplaceContent As Label
+    Friend WithEvents TB_FindContent As TextBox
+    Friend WithEvents TB_ReplaceContent As TextBox
+    Friend WithEvents BTN_Next As Button
+    Friend WithEvents BTN_Replace As Button
+    Friend WithEvents BTN_ReplaceAll As Button
+    Friend WithEvents BTN_Cancel As Button
+    Friend WithEvents CB_IsCaseSensitive As CheckBox
+    Friend WithEvents CB_IsLoop As CheckBox
 End Class
--- ReplaceDialog.vb
+++ ReplaceDialog.vb
@@ -1,3 +1,9 @@
 ﻿Public Class ReplaceDialog
 
+    ' 监听键盘的按键
+    Private Sub KeysClick(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Enter Then  ' 【回车键】被按下时
+
+        End If
+    End Sub
 End Class
